f748d818c4b1722e7bb6b5b9f9a4ab0ff060c0ac
Sai Mutyala <sai.mutyala@wwex.com>
2023-04-10 00:53:28 -0500

    Day 9

---
+++ 48.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        i=0
+        j=len(matrix)-1
+        n=len(matrix)
+        while j>i:
+            matrix[i],matrix[j]=matrix[j],matrix[i]
+            i+=1
+            j-=1
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
