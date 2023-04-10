66cdc37f5f607cc3cf45d70052de7f74167970e2
Sai Mutyala <sai.mutyala@wwex.com>
2023-04-10 01:30:34 -0500

    Day 11

---
+++ 13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        ans=0
+        for ch in s:
+            ans=ans+roman[ch]
+        return ans
+
