588b7c8c5f7b196afc792cd68d8e9d2d52bf35e1
sai krishna <skrishna383@gmail.com>
2023-07-13 04:10:28 -0500

    Day 50 Signed-off-by: sai krishna <skrishna383@gmail.com>

--- 208.py
+++ 208.py
@@ -1,3 +1,45 @@
+class TrieNode :
+    def __init__(self):
+        self.children={}
+        self.endofword=False
+
+class Trie:
+
+    def __init__(self,val="",left=None,right=None):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr=self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch]=TrieNode()
+            curr=curr.children[ch]
+        curr.endofword=True
+
+    def search(self, word: str) -> bool:
+        curr=self.root
+        for ch in word:
+            if ch not in curr.children:
+                return False
+            curr=curr.children[ch]
+        return curr.endofword
+
+    def startsWith(self, prefix: str) -> bool:
+        curr=self.root
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+            curr=curr.children[ch]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+#worst implimentation
 class Trie:
 
     def __init__(self,val="",left=None,right=None):
